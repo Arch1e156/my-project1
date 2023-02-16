6e35ee72b1dfdc621835670e0ed732869ee4ffd2
Egor <yegorik2003@gmail.com>
2023-02-16 15:19:45 +0200

    1 Js task

--- js/script.js
+++ js/script.js
@@ -1,25 +1,41 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
 "use strict";
 
-// console.log(4 + +"5");
+let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?", "");
 
-// let incr = 10,
-//     decr = 10;
+let lastFilmAnswer = prompt("Один из последних просмотренных фильмов?", "");
+let lastFilmScore = prompt("На сколько оцените его?");
 
-// // ++incr;
-// // --decr;
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        lastFilmAnswer: lastFilmScore
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// console.log(++incr);
-// console.log(--decr);
-
-// console.log(5 % 2);
-
-console.log(2 + 2 * 2 !== '8');
-
-// && i
-
-// || ili
-
-const isChecked = true,
-    isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(personalMovieDB);
